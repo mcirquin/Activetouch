6b4e7dde98dadbafd20b3c63ba68fc7fd9ca5654
idommisse <60976535+idommisse@users.noreply.github.com>
2022-04-06 14:43:00 +0200

    Update at_importonealigned.m

--- at_importonealigned.m
+++ at_importonealigned.m
@@ -1,17 +1,18 @@
 %Creates 2 tables (GF and LF) containing the alligned data
-%for each trial of each participant
+%for each trial of one participant 
+%!!!use data files without the 2 first blocks!!!
 
 trials = 108;
 nparticipants = 1;
 threshold = 2; %Valeur threshold pour l'alignement des courbes semon la LF 
 tstart = 2000;
 tsteps = 2759;  %nombre de pas de temps 
-GFtable = zeros(tsteps-tstart+1,nparticipants*trials); %matrice reprenant toutes les données de GF pour chaque essai pour chaque participant
-LFtable = zeros(tsteps-tstart+1,nparticipants*trials); %matrice reprenant toutes les données de LF pour chaque essai pour chaque participant
+GFtable = zeros(tsteps-tstart+1,nparticipants*trials); %matrice reprenant toutes les donnÃ©es de GF pour chaque essai pour chaque participant
+LFtable = zeros(tsteps-tstart+1,nparticipants*trials); %matrice reprenant toutes les donnÃ©es de LF pour chaque essai pour chaque participant
 l = 1;
  
 
-sujet = 'PLefèvre_14_02_2022'
+sujet = 'PLefÃ¨vre_14_02_2022'
 filelist=dir(sujet); 
 filelist= filelist(~[filelist.isdir]);
 [~,idx] = sort([filelist.datenum]);
@@ -20,8 +21,8 @@ nfiles=length(filelist);
    
 for i=1:108
     x = at_import(filelist(i).name, 'freqFiltForces', 20);
-    GFtable(:,l)=x.gf(tstart:tsteps); %extraction et stockage données de GF pour l'essai dans la matrice globale
-    LFtable(:,l)=x.lf(tstart:tsteps); %extraction et stockage données de LF pour l'essai dans la matrice globale
+    GFtable(:,l)=x.gf(tstart:tsteps); %extraction et stockage donnÃ©es de GF pour l'essai dans la matrice globale
+    LFtable(:,l)=x.lf(tstart:tsteps); %extraction et stockage donnÃ©es de LF pour l'essai dans la matrice globale
     l = l+1;
 end
 
