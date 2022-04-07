07c72ef4879bce30712d1567063d7e15d3be4bac
idommisse <60976535+idommisse@users.noreply.github.com>
2022-04-07 17:05:23 +0200

    Update stabGFcatchverre.m

--- stabGFcatchverre.m
+++ stabGFcatchverre.m
@@ -1,6 +1,6 @@
-%Graphe max GF vs Max weight LF, Max weight HF, Min weight LF, Min weight HF
+%Graphe mean stab GF vs Max weight LF, Max weight HF, Min weight LF, Min weight HF
 function stabLF = stabGFcatchverre(meanstabGFmatrix, nparticipants)
-%% Max LF catch verres
+%% GF LF catch verres
 nessais=4;
 maxstabLF=zeros(nessais, nparticipants);
 maxstabLF(1,:)=meanstabGFmatrix(13,:);
@@ -10,10 +10,10 @@ maxstabLF(4,:)=meanstabGFmatrix(91,:);
 meanmaxstabLFsub=mean(maxstabLF,2);%donne un tableau de 4 résultats donc moyennes des 4 essais
 meanmaxstabLF=mean(meanmaxstabLFsub);
 
-meanmaxstabLFparticipant = mean(maxstabLF);
+meanmaxstabLFparticipants = mean(maxstabLF);
 
 
-%% Max HF catch verres
+%% GF HF catch verres
 nessaisbis=3;
 maxstabHF=zeros(nessaisbis, nparticipants);
 maxstabHF(1,:)=meanstabGFmatrix(67,:);
@@ -22,9 +22,9 @@ maxstabHF(3,:)=meanstabGFmatrix(109,:);
 meanmaxstabHFsub=mean(maxstabHF,2);
 meanmaxstabHF=mean(meanmaxstabHFsub);
 
-meanmaxstabHFparticipant = mean(maxstabHF);
+meanmaxstabHFparticipants = mean(maxstabHF);
 
-%% Min LF catch verres
+%% GF LF catch verres
 minstabLF=zeros(nessaisbis, nparticipants);
 minstabLF(1,:)=meanstabGFmatrix(43,:);
 minstabLF(2,:)=meanstabGFmatrix(55,:);
@@ -32,9 +32,9 @@ minstabLF(3,:)=meanstabGFmatrix(103,:);
 meanminstabLFsub=mean(minstabLF, 2);
 meanminstabLF=mean(meanminstabLFsub);
 
-meanminstabLFparticipant = mean(minstabLF);
+meanminstabLFparticipants = mean(minstabLF);
 
-%% Min HF catch verres
+%% GF HF catch verres
 minstabHF=zeros(nessais, nparticipants);
 minstabHF(1,:)=meanstabGFmatrix(19,:);
 minstabHF(2,:)=meanstabGFmatrix(31,:);
@@ -43,7 +43,7 @@ minstabHF(4,:)=meanstabGFmatrix(97,:);
 meanminstabHFsub=mean(minstabHF, 2); %moyenne pour tous les sujets
 meanminstabHF=mean(meanminstabHFsub);%moyenne des moyennes de tous les sujets
 
-meanminstabHFparticipant = mean(minstabHF);
+meanminstabHFparticipants = mean(minstabHF);
 
 %% Plot des bars sur base des moyennes de tous les essais et de tous les participants
 figure(1)
@@ -53,19 +53,20 @@ set(gca,'XTickLabel',{'Maximal weight';'Minimal weight'});
 b(1).FaceColor = [0.9290 0.6940 0.1250];
 b(2).FaceColor = [0.8500 0.3250 0.0980];
 legend({'Low friction', 'High friction'},'Location','northeast')
-title('Mean grip force during stabilization for friction catch trials - Elderly participants')
-%title('Grip force peak magnitude for friction catch trials - Young participants')
+%title('Mean grip force during stabilization for friction catch trials - Elderly participants')
+title('Mean grip force during stabilization for friction catch trials - Young participants')
 ylabel('Mean grip force(N)')
 
 
 %% Plot des lignes correspondant à la moyenne des essais pour chaque participants
+%{
 hold on
 
 xdata= get (b(2),'XData');
 xlim=get(gca,'xlim');
 
 xparticipantmaxLF=[xlim(1)+0.2, xlim(1)+0.47];
-y1 = meanmaxstabLFparticipant;
+y1 = meanmaxstabLFparticipants;
 for i=1:nparticipants
     figure(1); hold on;
     plot(xparticipantmaxLF,y1(i)*ones(size(xparticipantmaxLF)), 'k','HandleVisibility','off')
@@ -74,14 +75,14 @@ end
 hold on
 
 xparticipantmaxHF=[xlim(1)+0.5, xlim(1)+0.75];
-y2 = meanmaxstabHFparticipant;
+y2 = meanmaxstabHFparticipants;
 for i=1:nparticipants
     figure(1); hold on;
     plot(xparticipantmaxHF,y2(i)*ones(size(xparticipantmaxHF)), 'k','HandleVisibility','off')
 end
 
 xparticipantminLF=[1.52+0.2, 1.52+0.47];
-y3 = meanminstabLFparticipant;
+y3 = meanminstabLFparticipants;
 for i=1:nparticipants
     figure(1); hold on;
     plot(xparticipantminLF,y3(i)*ones(size(xparticipantminLF)), 'k','HandleVisibility','off')
@@ -90,16 +91,51 @@ end
 hold on
 
 xparticipantminHF=[1.52+0.5, 1.52+0.75];
-y4 = meanminstabHFparticipant;
+y4 = meanminstabHFparticipants;
 for i=1:nparticipants
     figure(1); hold on;
     plot(xparticipantminHF,y4(i)*ones(size(xparticipantminHF)), 'k','HandleVisibility','off')
 end
 
+%}
 
+%% Barplots with subject slopes
+%color cell (suppress colors if less participants!)
+C = {'k','b','r','g',[1 0.9 0.1],[.5 .6 .7],[.8 .2 .6],[0.7 0.5 0.9],'c','m',[0.9 0.4 1],[0.2 0.8 0.7],[0.7 0.4 0.1],[0.4 0.55 0.8],[0.3 0.6 0.3]}; % Cell array of colros.
+xdata= get (b(2),'XData');
+xoffset1 = -0.14;
+xoffset2 = 0.14;
+
+hold on
+y1 = meanmaxstabLFparticipants;
+y2 = meanmaxstabHFparticipants;
+for i=1:nparticipants
+    figure(1); hold on;
+    plot(xdata(1)+xoffset1,y1(i),'.', 'MarkerSize',15,'color',C{i},'HandleVisibility','off')
+
+    hold on
+    figure(1); hold on;
+    plot(xdata(1)+xoffset2,y2(i),'.', 'MarkerSize',15,'color',C{i},'HandleVisibility','off')
+    
+    line([xdata(1)+xoffset1,xdata(1)+xoffset2], [y1(i), y2(i)], 'Color', C{i},'HandleVisibility','off')
+end
+
+hold on
+y3 = meanminstabLFparticipants;
+y4 = meanminstabHFparticipants;
+for i=1:nparticipants
+    figure(1); hold on;
+    plot(xdata(2)+xoffset1,y3(i),'.', 'MarkerSize',15,'color',C{i},'HandleVisibility','off')
+
+    hold on
+    figure(1); hold on;
+    plot(xdata(2)+xoffset2,y4(i),'.', 'MarkerSize',15,'color',C{i},'HandleVisibility','off')
+    
+    line([xdata(2)+xoffset1,xdata(2)+xoffset2], [y3(i), y4(i)], 'Color', C{i},'HandleVisibility','off')
+end
 
 %% Code pour avoir tous les points data superposés sur le barplot (Elderly participants)
-
+%{
 
 xdata= get (b(2),'XData');
 ydata= get (b(2),'YData');
@@ -181,7 +217,7 @@ plot(xdata(2)+xoffset,D(31:40),'k.','HandleVisibility','off');
 hold on 
 xoffset = 0.18; 
 plot(xdata(2)+xoffset,D(41:52),'k.', 'HandleVisibility','off');
-
+%}
 %% Code pour avoir tous les points data superposés sur le barplot (Young participants)
 %{
 hold on;
@@ -208,8 +244,6 @@ plot(xdata(1)+xoffset,A(42:48),'k.', 'HandleVisibility','off');
 hold on 
 xoffset = -0.18; % manual set of get from properties of bar handle
 plot(xdata(1)+xoffset,A(49:60),'k.', 'HandleVisibility','off');
-
-
 hold on;
 xoffset = 0.11; % manual set of get from properties of bar handle
 plot(xdata(1)+xoffset,B(1:5),'k.','HandleVisibility','off');
@@ -234,8 +268,6 @@ plot(xdata(1)+xoffset,B(36:40),'k.', 'HandleVisibility','off');
 hold on 
 xoffset = 0.18; % manual set of get from properties of bar handle
 plot(xdata(1)+xoffset,B(41:45),'k.', 'HandleVisibility','off');
-
-
 hold on;
 xoffset = -0.11; % manual set of get from properties of bar handle
 plot(xdata(2)+xoffset,C(1:5),'k.','HandleVisibility','off');
@@ -260,8 +292,6 @@ plot(xdata(2)+xoffset,C(36:40),'k.', 'HandleVisibility','off');
 hold on 
 xoffset = -0.18; % manual set of get from properties of bar handle
 plot(xdata(2)+xoffset,C(41:45),'k.', 'HandleVisibility','off');
-
-
 hold on;
 xoffset = 0.11; % manual set of get from properties of bar handle
 plot(xdata(2)+xoffset,D(1:6),'k.','HandleVisibility','off');
@@ -302,8 +332,6 @@ text(1.14,y1right,labels2,'HorizontalAlignment','center',...
     'VerticalAlignment','bottom')
 text(2.14,y2right,labels3,'HorizontalAlignment','center',...
     'VerticalAlignment','bottom')
-
-
 ytips2= get (b(1),'YData')
 y1left = ytips2(1)
 y2left = ytips2(2)
