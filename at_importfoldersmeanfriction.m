9dbd6a3aabedc26f339e21047cbd57ca5668fc56
idommisse <60976535+idommisse@users.noreply.github.com>
2022-04-15 16:33:02 +0200

    Update at_importfoldersmeanfriction.m

--- at_importfoldersmeanfriction.m
+++ at_importfoldersmeanfriction.m
@@ -12,6 +12,7 @@ Fint = [Fmin:Fmax];
 k=1; %loop parameter
 Rdlim = 10; %limit for acceptable relative difference between the 2 friction coefficients
 pnames = string.empty; %array to store names of participants with Rd below limit
+Rdvector = [];
 
 D = 'Data Friction 18-35';
 S = dir(fullfile(D, '*'));
@@ -19,6 +20,7 @@ N = setdiff({S([S.isdir]).name},{'.','..'}); % list of subfolders of D.
 
 x=linspace(0,1.6);
 y=linspace(0,1.6);
+figure(1);
 h1=plot(x,y,'Color',[0.2 0.2 0.2]); %identity line (x=y)
 
 for ii = 1:numel(N)  %loop going through the folders 
@@ -36,6 +38,7 @@ for ii = 1:numel(N)  %loop going through the folders
     
     %relative difference in friction
     Rd = abs(meanCF_HF-meanCF_LF)/min(meanCF_HF,meanCF_LF)*100;
+    Rdvector(ii) = Rd;
     
     %Identification of participants with relative difference in friction
     %lower than 10%
@@ -49,7 +52,7 @@ for ii = 1:numel(N)  %loop going through the folders
     %graph plots
     pos1 = [0.1 0.15 0.5 0.7];
     subplot('Position',pos1);
-    hold on;
+    figure(1);hold on;
     C = {'k','b','r','g',[1 0.9 0.1],[.5 .6 .7],[.8 .2 .6],[0.7 0.5 0.9],'c','m',[0.9 0.4 1],[0.2 0.8 0.7],[0.7 0.4 0.1],[0.4 0.55 0.8],[0.3 0.6 0.3]}; % Cell array of colors.
     h2= plot(meanCF_LF,meanCF_HF,'.', 'MarkerSize',10,'Color', C{ii});
     h1=plot(x,y,'Color',[0.2 0.2 0.2]); %identity line (x=y)
@@ -63,9 +66,14 @@ for ii = 1:numel(N)  %loop going through the folders
     yticks([0 0.2 0.4 0.6 0.8 1 1.2 1.4 1.6]);
     
     pos2 = [0.7 0.15 0.1 0.7];
-    subplot('Position',pos2); hold on;
-    h1 = plot(1,Rd,'.', 'MarkerSize',10,'Color', C{ii});
+    subplot('Position',pos2); 
+    figure(1);hold on;
+    h1=plot(1,Rd,'.', 'MarkerSize',10,'Color', C{ii});
     set(gca,'XTick',[])
     set(gca, 'YDir','reverse')
     ylabel('Relative difference in Friction [%]')  
 end
+figure(1); hold on;
+h2=errorbar(1,mean(Rdvector),std(Rdvector),'-*', 'Color', [0.5 0.5 0.5], 'LineWidth', 0.4, 'Markersize', 10);
+hold on;
+h=[h(1);h(2)];
