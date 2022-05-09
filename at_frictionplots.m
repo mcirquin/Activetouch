e9e804e4c20b40db66c10501412c39ec1d20d1d1
idommisse <60976535+idommisse@users.noreply.github.com>
2022-05-09 14:19:39 +0200

    Update at_frictionplots.m

--- at_frictionplots.m
+++ at_frictionplots.m
@@ -1,4 +1,4 @@
-function [indexglass1, indexblue] = at_frictionplots(dataDir, flist, tsteps)
+function [indexglass1, thumbglass1, indexblue, thumbblue] = at_frictionplots(dataDir, flist, tsteps)
 %Fonction qui calcule les frictions d'un participant grâce aux 6 fichiers
 %de mesure de friction
 %dataDir est le nom du dossier, par exemple 'GBrandsteert', filelist contient les
@@ -12,6 +12,7 @@ clear h;
 cond_tab={'Weak','Medium','Strong'};
 
 %% %% Boucle pour le calcul de la friction de l'échantillon "non-glissant"
+%index CF
 for i=1:3
     if i==1
         forcesFile = strcat(flist(i).name);
@@ -48,7 +49,45 @@ end
 
 indexblue=get_mu_fit(indexblue);
 
+%thumb CF
+for i=1:3
+    if i==1
+        forcesFile = strcat(flist(i).name);
+    elseif i==2
+        forcesFile = strcat(flist(i).name);
+    elseif i==3
+        forcesFile = strcat(flist(i).name);
+    end
+    data = at_import(fullfile(dataDir,forcesFile));
+    
+    cond=cond_tab{i};
+    
+    data=data(range,:);
+    y_COP=(data.copy_led+33)/1000;
+    ft=[-(data.fx_led) -(data.fy_led)];
+    NF=data.fz_led;
+    
+    if i==1
+        x=1:length(NF);
+        NF=NF-interp1([1 length(NF)],[NF(1) NF(end)],x)';
+        ft(:,1)=ft(:,1)-interp1([1 length(NF)],[ft(1,1) ft(end,1)],x)';
+        ft(:,2)=ft(:,2)-interp1([1 length(NF)],[ft(1,2) ft(end,2)],x)';
+    end
+    
+    [mu_l,slip_thumb_l,search_zones_l] = get_mu_points(y_COP,ft,NF,'fs_science',200,'y_thresh',0.02);
+    thumbblue.conditions{i}=cond;
+    thumbblue.(cond).NF=NF;
+    thumbblue.(cond).TF=ft;
+    thumbblue.(cond).mu=mu_l;
+    thumbblue.(cond).slip_indexes=slip_thumb_l;
+    thumbblue.(cond).search_zones=search_zones_l;
+
+end
+
+thumbblue=get_mu_fit(thumbblue);
+
 %% Boucle pour le calcul de la friction de l'échantillon "glissant"
+%index CF
 for i=1:3
     if i==1
         forcesFile = strcat(flist(i+3).name);
@@ -86,6 +125,40 @@ end
 
 indexglass1=get_mu_fit(index);
 
+%thumb CF
+for i=1:3
+    if i==1
+        forcesFile = strcat(flist(i+3).name);
+    elseif i==2
+        forcesFile = strcat(flist(i+3).name);
+    else
+        forcesFile = strcat(flist(i+3).name);
+    end
+    data = at_import(fullfile(dataDir,forcesFile),true,40,40,40,10);
+    
+    cond=cond_tab{i};
+    
+    data=data(range,:);
+    y_COP=(data.copy_led+33)/1000; % Ajustement du COP pour que zéro soit le centre de la vitre
+    ft=[-(data.fx_led) -(data.fy_led)];
+    NF=data.fz_led;
+    
+    if correct_forces %Correction de la dérive des capteurs
+        x=1:length(NF);
+        NF=NF-interp1([1 length(NF)],[NF(1) NF(end)],x)';
+        ft(:,1)=ft(:,1)-interp1([1 length(NF)],[ft(1,1) ft(end,1)],x)';
+        ft(:,2)=ft(:,2)-interp1([1 length(NF)],[ft(1,2) ft(end,2)],x)';
+    end
+    
+    
+    [mu_l,slip_thumb_l,search_zones_l] = get_mu_points(y_COP,ft,NF,'fs_science',200,'y_thresh',0.02); %previously,y=0.025
+    thumb.conditions{i}=cond;
+    thumb.(cond).NF=NF;
+    thumb.(cond).TF=ft;
+    thumb.(cond).mu=mu_l;
+    thumb.(cond).slip_indexes=slip_thumb_l;
+    thumb.(cond).search_zones=search_zones_l;
 
+end
 
-
+thumbglass1=get_mu_fit(thumb);
